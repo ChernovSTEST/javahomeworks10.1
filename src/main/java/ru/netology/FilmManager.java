592f49d9d6c2816d08e7fdfc6fb557ae2d4143a5
Test <test@test.com>
2023-05-03 16:52:53 +0300

    Ended

--- src/main/java/ru/netology/FilmManager.java
+++ src/main/java/ru/netology/FilmManager.java
@@ -1,2 +1,41 @@
-package ru.netology;public class FilmManager {
+package ru.netology;
+
+public class FilmManager {
+    private int limit;
+    private String[] films;
+
+    public FilmManager() {
+        this.limit = 5;
+        this.films = new String[0];
+    }
+
+    public FilmManager(int limit) {
+        this.limit = limit;
+        this.films = new String[0];
+    }
+
+    public void addFilm(String film) {
+        String[] newFilms = new String[this.films.length + 1];
+        System.arraycopy(this.films, 0, newFilms, 0, this.films.length);
+        newFilms[newFilms.length - 1] = film;
+        this.films = newFilms;
+    }
+
+    public String[] findAll() {
+        return this.films;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (this.films.length > this.limit) {
+            resultLength = this.limit;
+        } else {
+            resultLength = this.films.length;
+        }
+        String[] result = new String[resultLength];
+        for (int i = 0; i < resultLength; i++) {
+            result[i] = this.films[this.films.length - 1 - i];
+        }
+        return result;
+    }
 }
--- src/test/java/ru/netology/FilmManagerTest.java
+++ src/test/java/ru/netology/FilmManagerTest.java
@@ -1,2 +1,49 @@
-package ru.netology;public class FilmManagerTest {
+package ru.netology;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilmManagerTest {
+
+    @Test
+    public void testAddFilm() {
+        FilmManager manager = new FilmManager();
+        manager.addFilm("Film 1");
+        assertArrayEquals(new String[]{"Film 1"}, manager.findAll());
+
+        manager.addFilm("Film 2");
+        assertArrayEquals(new String[]{"Film 1", "Film 2"}, manager.findAll());
+
+        manager.addFilm("Film 3");
+        assertArrayEquals(new String[]{"Film 1", "Film 2", "Film 3"}, manager.findAll());
+    }
+
+    @Test
+    public void testFindAll() {
+        FilmManager manager = new FilmManager();
+        assertArrayEquals(new String[0], manager.findAll());
+
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        assertArrayEquals(new String[]{"Film 1", "Film 2"}, manager.findAll());
+    }
+
+    @Test
+    public void testFindLast() {
+        FilmManager manager = new FilmManager(2);
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+
+        assertArrayEquals(new String[]{"Film 3", "Film 2"}, manager.findLast());
+
+        manager = new FilmManager();
+        manager.addFilm("Film 1");
+        assertArrayEquals(new String[]{"Film 1"}, manager.findLast());
+
+        manager = new FilmManager(1);
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        assertArrayEquals(new String[]{"Film 2"}, manager.findLast());
+    }
 }
+
